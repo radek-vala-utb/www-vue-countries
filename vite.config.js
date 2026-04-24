165d0bb7c2eb72d42ce839b6b066cbc6960fe5d1
Radek Vala <radek.www@gmail.com>
2026-04-24 15:55:13 +0200

    Fixed deploy paths

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,6 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
 export default defineConfig({
-  base: '/www-vue-countries/dist/',
+  base: '/www-vue-countries/',
   plugins: [vue()],
 })
